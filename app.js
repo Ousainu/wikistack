47f5807ab6dae503eaad313338a0f4f2657d70ed
Ousainu Jabbi <ousainujabbi@gmail.com>
2020-04-30 13:03:33 -0400

    Connecting to postgres

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const main = require('./views/main');
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({extend: false}));
+
+app.get("/", async (req, res) => {
+    res.send(main());
+})
+
+const PORT = 1337;
+
+app.listen(PORT, () => {
+    console.log('Listening on port: ', PORT);
+});
