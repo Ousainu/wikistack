1ab9e91a7680af410f5222abbb61d703634f5979
Kristy Li <kristyli20.0@gmail.com>
2020-04-30 16:37:09 -0400

    Create sync functions for Page and Sync

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
-const main = require('./views/main');
+const main = require("./views/main");
+// const { db } = require('./models');
+const { Page, User } = require("./models");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.static(__dirname + "/public"));
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", async (req, res) => {
-    res.send(main());
-})
+  res.send(main());
+});
 
-const PORT = 1337;
+// db.authenticate().then(() => {
+//     console.log('connected to the database');
+// });
+
+const init = async () => {
+    await Page.sync({force: true});
+    await User.sync({force: true});
+};
+
+init();
+
+const PORT = 1338;
 
 app.listen(PORT, () => {
-    console.log('Listening on port: ', PORT);
+  console.log("Listening on port: ", PORT);
 });
